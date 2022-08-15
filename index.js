1fa2c7846ef6353677cf2f9c66286729886ad4fb
grantbergland <grantbergland@gmail.com>
2022-08-15 14:12:36 -0500

    commit tree cb0be0df2c6a0efce2ea38d2d7f4187ffe546b8d

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  var re = /[^A-Za-z0-9]/g;
+  word = word.toLowerCase().replace(re, '');
+  var len = word.length;
+  for (var i = 0; i < len/2; i++) {
+    if (word[i] !== word[len - 1 - i]) {
+        return false;
+    }
+  }
+  return true;
+ }
 
 /* 
-  Add your pseudocode here
+Add your pseudocode here
 */
 
 /*
-  Add written explanation of your solution here
+Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+// add your own custom tests in here
+console.log("Expecting: true");
+console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
+
+function fetchPokemon(){
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+   .then(response => response.json())
+   .then(function(allpokemon){
+   allpokemon.results.forEach(function(pokemon){
+     fetchPokemonData(pokemon); 
+   })
+  })
+ }
+
+
